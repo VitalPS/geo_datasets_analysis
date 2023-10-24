7c788b6e5c239b40a7c28413a35f7dce59fa7cfb
Patrik da Silva Vital <patrikdasilvavital@gmail.com>
2023-10-24 16:41:54 -0300

    rainplot added, bug fixes and code cleaning

--- geo_transcriptome_analysis.R
+++ geo_transcriptome_analysis.R
@@ -1,33 +1,24 @@
-# ABRINDO PACOTES NECESSÁRIOS
+### ABRINDO PACOTES NECESSÁRIOS
 
-lapply(list("GEOquery", # baixar dados do GEO
-            "DESeq2", # análise estatística
-            "edgeR", # análise estatística
-            "tidyverse", # manipular dados)
-            "ggplot2", # fazer gráficos
-            "ggvolc"
-            ), 
-       require,
-       character.only = T)
+lapply(list("tidyverse", # manipular dados, fazer graficos, etc ...
+            "ggvolc",  # volcano plot
+            require,
+            character.only = T))
 
 
 
-# FAZENDO DOWNLOAD DOS DADOS DO EXPERIMENTO
 
-raw_data <- getGEO("GSE105261", GSEMatrix = T)
+### FAZENDO DOWNLOAD DOS DADOS DO EXPERIMENTO
+
+raw_data <- GEOquery::getGEO("GSE105261", GSEMatrix = T)
 
 raw_data <- raw_data[[1]]
-
-
-geo <- getGEO("GSE105261", GSEMatrix = T)
-
-geo <- raw_data[[1]]
-
 
 
 
 ### RECUPERANDO METADADOS
 
+# obtendo metadados do arquivo GEO
 raw_metadata <- raw_data@phenoData@data
 
 metadata <- raw_metadata %>% 
@@ -37,7 +28,7 @@ metadata <- raw_metadata %>%
   dplyr::rename(samples = 'geo_accession') %>%
   dplyr::rename(tissue = 'tissue:ch1') %>%
   # renomar observações da coluna tecido
-  mutate(tissue = case_match(
+  dplyr::mutate(tissue = dplyr::case_match(
     tissue,
     "Benign tissue" ~ "benign",
     "Normal renal tissue" ~ "normal",
@@ -46,18 +37,20 @@ metadata <- raw_metadata %>%
   # filtrar tecidos normais e tumores primários
   dplyr::filter(tissue %in% c("metastatic", "primary")) %>%
   # transformando coluna tecido para fator
-  mutate(tissue = factor(.$tissue)) %>%
+  dplyr::mutate(tissue = base::factor(.$tissue)) %>%
   # adicionar identificadores para a coluna tecido
-  mutate(color = case_match(
+  dplyr::mutate(color = dplyr::case_match(
     tissue,
     "metastatic" ~ "blue",
     "primary" ~ "green"))
 
-
+# remove raw_metadata object
+base::rm("raw_metadata")
 
 
 ### RECUPERANDOS PROBES IDS
 
+# obtendo características das probes
 feature_data <- raw_data@featureData@data
 
 # filtrando IDs e nomes dos genes
@@ -69,10 +62,10 @@ feature_data[feature_data == ""] <- NA
 
 ### SUBSTITUINDO PROBES IDS PELOS NOMES DOS GENES
 
-# Recuperando dados de expressão
-raw_expression_data <- as.data.frame(exprs(raw_data))
-
-expression_data <- raw_expression_data %>%
+# obtendo dados de expressão
+expression_data <- Biobase::exprs(raw_data) %>%
+  # transforma para dataframe
+  base::as.data.frame() %>%
   # cria uma coluna com os simbolos
   tibble::rownames_to_column(var = "ID") %>% 
   # juntando probes ids com dados de expressão
@@ -84,246 +77,223 @@ expression_data <- raw_expression_data %>%
   # removendo itens duplicados
   dplyr::filter(!duplicated(.$gene)) %>%
   # removendo os NAs
-  na.omit()
-
+  stats::na.omit()
   
 
 
-### NORMALIZAR E FAZER ANÁLISE ESTATÍSTICA
+### SELECIONANDO PACIENTES DE INTERESSE
 
-processed_expression_data <- expression_data %>%
-  `row.names<-`(.$gene) %>%
+filtered_expression_data <- expression_data %>%
+  # atribuindo genes aos nomes das linhas
+  base::`row.names<-`(.$gene) %>%
+  # excluindo coluna gene
   dplyr::select(- gene) %>%
+  # filtrando pacientes selecionados nos metadados
   dplyr::select(metadata$samples)
   
-
-
 # checando se todas amostras dos metadados são as mesmas dos dados de expressão
-all(metadata$samples %in% colnames(processed_expression_data))
-
+all(metadata$samples %in% colnames(filtered_expression_data))
 
 # checando se todas amostras dos metadados estão na mesma ordem dos dados de expressão
-all(metadata$samples == colnames(processed_expression_data))
+all(metadata$samples == colnames(filtered_expression_data))
 
 
-### ANÁLISE ESTATÍSTICA
 
-# criando um objeto dgeList
-dge <- DGEList(processed_expression_data)
+### CHECANDO DISTRIBUIÇÃO DOS DADOS
+
+# criando um objeto dgeList para análise com o pacote edgeR
+dge_object <- edgeR::DGEList(filtered_expression_data)
 
 # adicionando os grupos ao dgeList como fator
-dge$samples$group <- factor(metadata$tissue)
+dge_object$samples$group <- base::factor(metadata$tissue)
 
-## Plotar gráfico PCA
-plotMDS(dge, col = metadata$color, pch=16)
+# checando diferenças nos grupos selecionados para analise (gráfico PCA)
+limma::plotMDS(dge_object, 
+               col = metadata$color, 
+               pch=16)
 
+# converter numero de counts em Counts Per Million (cpm)
+mycpm <- edgeR::cpm(filtered_expression_data)
 
+# filtrando amostras que tenham cpm muito baixo (cpm < 0.5)
+  # thresh <- mycpm > .5
 
+# convertendo cpm em log2 cpm
+logcounts <- edgeR::cpm(mycpm, log=TRUE)
 
-mycpm <- cpm(processed_expression_data)
+# checando a distribuição dos dados de contagem
+BiocGenerics::boxplot(logcounts, xlab="", ylab="Log2 counts per million",las=2)
 
-thresh <- mycpm > .5
-
-logcounts <- cpm(mycpm,log=TRUE)
-
-boxplot(logcounts, xlab="", ylab="Log2 counts per million",las=2)
+# remove objetos mycpm e logcounts
+rm(list = c("mycpm", "logcounts"))
 
 
 
 ### NORMALIZAR DADOS
 
-dge_normalizado <- calcNormFactors(dge)
+# normaliza os dados de contagem do objeto dgelist (TMM)
+normalized_dge_object <- edgeR::calcNormFactors(dge_object)
 
-plotMD(dge_normalizado,column = 7)
-
+# checa a distribuição da expressão genica normalizada em uma amostra
+limma::plotMD(normalized_dge_object, 
+              column = 7)
 
 
 
 ### EXPRESSÃO DIFERENCIAL
 
-# Fazer matriz dummy para os grupos
-design <- model.matrix(~ 0 + metadata$tissue)
-# Renomeando colunas
-colnames(design) <- levels(metadata$tissue) 
+# fazer matriz dummy para os grupos de estudo
+design <- stats::model.matrix(~ 0 + metadata$tissue)
+# renomeando colunas para o nome dos grupos de estudo
+colnames(design) <- base::levels(metadata$tissue) 
 
 
-# Criando um objeto Voom
-voom_object <- voom(dge_normalizado,
-                    design = design,
-                    plot = TRUE)
+# criando um objeto Voom (transforma os dados em logCPM levando em consideração 
+# a variancia media da amostra) para ser analisado pela função lmFit
+voom_object <- limma::voom(normalized_dge_object,
+                          design = design,
+                          plot = TRUE)
 
-# Modelo linear
-fit <- lmFit(voom_object)
+# ajustar modelo linear
+fit <- limma::lmFit(voom_object)
 
-# Cria uma matriz de comparação entre diferentes grupos
-cont.matrix <- makeContrasts(primary_vs_metastatic = metastatic - primary,
-                              levels = design)
+# cria uma matriz de comparação entre os grupos de estudo
+contrast_matrix <- limma::makeContrasts(primary_vs_metastatic = metastatic - primary,
+                                        levels = design)
 
-# 
-fit.cont <- contrasts.fit(fit, cont.matrix)
+# computa coeficientes e erros padrões em um modelo linear, dada uma matriz de comparação
+statistical_analysis <- limma::contrasts.fit(fit = fit, 
+                                 contrasts = contrast_matrix)
 
-fit.cont <- eBayes(fit.cont)
+# análisess estatísticas (T, F, )
+statistical_analysis <- limma::eBayes(statistical_analysis)
 
-summa.fit <- decideTests(fit.cont)
-summary(summa.fit)
+# calcula quais genes estão diferencialmente expressos, dados os contrastes
+summa.fit <- limma::decideTests(statistical_analysis)
 
-pvalues <- fit.cont$p.value
+# retorna quantos genes estão diferencialmente expressos (up, down ou não significativo)
+base::summary(statistical_analysis)
+
+pvalues <- statistical_analysis$p.value
 
 # arredonda valores da tabela para 3 digitos pós virgula
 options(digits=3)
 
 # gera tabela com valores de logFC, expressão média, t, pvalor, pvalor ajustado
-final_table <- topTable(fit.cont)
+top_genes_table <- limma::topTable(statistical_analysis)
 
-final_table_processed <- final_table %>%
-  rename(pvalue = P.Value) %>%
-  rename(log2FoldChange = logFC)
-
-
-
-ggvolc(final_table_processed,
-       add_seg = T )
+# plota um volcano plot dos top 10 genes diferencialmente expressos
+top_genes_table %>%
+  dplyr::rename(pvalue = P.Value) %>%
+  dplyr::rename(log2FoldChange = logFC) %>%
+  ggvolc::ggvolc(add_seg = T )
        
-plot %>%
-  genes_table(final_table_processed)
-
-# Criando um dataset de expressão diferencial
-# dds <- DESeqDataSetFromMatrix(
-#   countData = round(processed_expression_data),
-#   colData = metadata,
-#   design = ~ tissue)
-
-# dds <- estimateDispersionsGeneEst(dds)
-# 
-# dispersions(dds) <- mcols(dds)$dispGeneEst
-
-# Setando controle basal
-# deseq_dataset$tissue <- relevel(
-#   deseq_dataset$tissue, 
-#   ref = "primary"
-#   )
-
-
-# Executando DESeq
-# dds <- DESeq()
-# 
-# dds_results < results(dds)
-
 
 
 ### MANIPULANDO DADOS DE EXPRESSÃO
 
-expression_data_final <- expression_data %>%
-  # reformatando dados para três colunas (gene [inaltarada], amostras e expressão)
-  tidyr::pivot_longer(names_to = 'samples', 
+
+graph_table <- filtered_expression_data %>%
+  # filtra os dados de expressão dos top 10 genes
+  dplyr::filter(rownames(.) %in% row.names(top_genes_table)) %>%
+  # transpoe a tabela
+  base::t() %>%
+  # transforma para data frame
+  base::as.data.frame() %>%
+  # transforma o nome das linhas e os transforma para uma coluna chamada samples
+  tibble::rownames_to_column(var = "samples") %>%
+  # transforma as variaveis samples, genes e expression em colunas mantendo samples como padrão
+  tidyr::pivot_longer(names_to = 'genes', 
                       values_to = "expression", 
-                      - gene) %>%
-  # juntando dados de expressão com metadados
-  dplyr::left_join(metadata, 
-                   by = "samples")
+                      - samples) %>%
+  # acrescenta os dados clínicos dos pacientes
+  dplyr::left_join(metadata, by = "samples") %>%
+  # exclui a coluna color
+  dplyr::select(-color)
+
+
+
+### PLOTANDO O RAINCLOUD PLOT
   
+# paleta de cores
+pal <- c("#A034F0", "#159090") # paleta de cores
 
+# plot base
+raincloud_plot <-
+  ggplot2::ggplot(graph_table, aes(x = genes, y = expression, fill = tissue)) +
+  PupillometryR::geom_flat_violin(
+    position = position_nudge(x = .1, y = 0),
+    adjust = 1.5,
+    trim = FALSE,
+    alpha = .5,
+    colour = NA
+  ) +
+  geom_boxplot(
+    outlier.shape = NA,
+    alpha = .5,
+    width = .1,
+    colour = "grey" # contorno do boxplot
+  ) +
+  geom_point(
+    aes(colour = tissue),
+    position = position_jitter(width = .03, height = 0),
+    alpha = 0.3,
+    shape = 20
+  )
 
+# plot mais nome dos eixos
+raincloud_plot <-
+  raincloud_plot +
+  ggplot2::scale_color_manual(values = pal) +
+  ggplot2::scale_fill_manual(values = pal) +
+  ggplot2::labs(
+    x = "Gene names",
+    y = "RNA counts",
+    title = "Transcriptome analysis of primary and metastasis ccRCC",
+    subtitle = "Distribution of the most significantly differentially expressed genes among primary and metastatic ccRCC tumors",
+    caption = "Nam HY,  *et al.* (2019) *Mol Cancer Res* DOI: 10.1158/1541-7786<br>Visualization: Vital PS and Siqueira AP"
+  )
 
+# plot mais personalizações
+raincloud_plot <-
+  raincloud_plot +
+  ggplot2::theme_minimal(base_family = "Zilla Slab", base_size = 12) +
+  ggplot2::theme(
+    panel.grid.minor = ggplot2::element_blank(),
+    # exclude grids
+    panel.grid.major = ggplot2::element_blank(),
+    # exclude grids
+    axis.ticks = ggplot2::element_blank(),
+    axis.text.x = ggplot2::element_text(family = "Roboto Mono"),
+    axis.text.y = ggplot2::element_text(color = "gray40",
+                                        size = 12),
+    axis.title.x = ggplot2::element_text(
+      margin = margin(t = 10),
+      size = 16,
+      color = "gray40"
+    ),
+    axis.title.y = ggplot2::element_text(
+      margin = margin(t = 10),
+      size = 16,
+      color = "gray40"
+    ),
+    plot.title = ggtext::element_markdown(face = "bold", size = 21),
+    plot.subtitle = ggplot2::element_text(
+      color = "grey40",
+      hjust = 0,
+      margin = margin(0, 0, 20, 0)
+    ) ,
+    plot.title.position = "plot",
+    plot.caption = ggtext::element_markdown(
+      color = "grey40",
+      lineheight = 1.2,
+      margin = margin(20, 0, 0, 0)
+    ),
+    plot.margin = margin(15, 15, 10, 15)
+  )
 
+# plota o raincloud plot
+raincloud_plot
 
-
-
-
-# 
-# # criar uma lista de referência simbolo/nome do gene
-# gene_simbols = data.frame(gene = unlist(mget(x = row.names(raw_expression_data),
-#                 envir = illuminaHumanv4SYMBOL)))
-# 
-# # ignorar simbolos sem nomes encontrados (valores NA)
-# gene_simbols <- na.omit(gene_simbols)
-# gene_simbols[, "row_names"] <- row.names(gene_simbols)
-# 
-# # renomeando simbolos
-# expression_data <- raw_expression_data %>%
-#   dplyr::inner_join(
-#     gene_simbols,
-#     by = "row_names"
-#   ) %>%
-#   `row.names<-`(.$gene) %>%
-#   dplyr::select(-c("row_names", "gene"))
-# 
-# duplicated(expression_data$gene)
-# 
-# 
-# 
-# # # log2 transform
-# # qx <-as.numeric(
-# #   quantile(ex, c(0., 0.25, 0.5, 0.75, 0.99, 1.0), 
-# #            na.rm = T))
-# # 
-# # LogC <- (qx[5] > 100) ||
-# #   (qx[6] - qx[1] > 50 && qx[2] > 0)
-# # 
-# # if (LogC) {
-# #   ex[which(ex <= 0)] <- NaN
-# #   ex <- log2(ex)
-# # }
-# 
-# 
-# # box-and-whisker plot
-# dev.new(width = 3 + ncol(gset) / 6, height = 5)
-# 
-# par(mar = c(7, 4, 2, 1))
-# title <- paste (
-#   "GSE105288", 
-#   "/", 
-#   annotation(gset), 
-#   sep = ""
-#   )
-# 
-# boxplot(
-#   ex,
-#   boxwex = 0.7,
-#   notch = T,
-#   main = title,
-#   outline = FALSE,
-#   las = 2
-# )
-# 
-# dev.off()
-# 
-# # expression value distribution plot
-# par(mar = c(4, 4, 2, 1))
-# title <- paste (
-#   "GSE105288", "/", 
-#   annotation(gset), 
-#   "value distribution", 
-#   sep = "")
-# 
-# plotDensities(ex, main = title, legend = F)
-# 
-# 
-# # mean-variance trend
-# ex <- na.omit(ex) # eliminate rows with NAs
-# 
-# plotSA(
-#   lmFit(ex), 
-#   main = "Mean variance trend, GSE105288"
-#   )
-# 
-# 
-# 
-# # # UMAP plot (multi-dimensional scaling)
-# # ex <- ex[!duplicated(ex),]  # remove duplicates
-# # ump <- umap(t(ex), n_neighbors = 15, random_state = 123)
-# # plot(
-# #   ump$layout,
-# #   main = "UMAP plot, nbrs=15",
-# #   xlab = "",
-# #   ylab = "",
-# #   pch = 20,
-# #   cex = 1.5
-# # )
-# # library("maptools")  # point labels without overlaps
-# # pointLabel(
-# #   ump$layout,
-# #   labels = rownames(ump$layout),
-# #   method = "SANN",
-# #   cex = 0.6
-# # )
+# salvar grafico como imagem
+ggsave('raincloudplot.png', width = 20, height = 10)
